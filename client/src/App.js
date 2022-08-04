8d5d04f5eed326cc4eed8e4a4ad876fda4ba4441
E132 <107452563+132E@users.noreply.github.com>
2022-08-03 21:36:08 -0500

    frontend is pulling from backend

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,24 @@ import CommentDetails from "./pages/CommentDetails";
 import BugDetails from "./pages/BugDetails";
 import FishDetails from "./pages/FishDetails";
 import React from "react";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { BASE_URL } from "./globals";
+import axios from "axios";
+
+import "./App.css";
 
 const App = () => {
+  const [bugs, setBugs] = useState([]);
+
+  useEffect(() => {
+    const getBugs = async () => {
+      const res = await axios.get(`${BASE_URL}/api/bugs`);
+      setBugs(res.data);
+    };
+    getBugs();
+  }, []);
+
   return (
     <div>
       <header>
